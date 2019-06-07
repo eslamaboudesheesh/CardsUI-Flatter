7cacc529e15254468c41e917043113f7eb86246b
eslam <eslamaboudesheesh@hotmail.com>
2019-06-07 04:43:41 +0200

    add splash screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:startagain/main.dart';
+import 'package:startagain/start-app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
